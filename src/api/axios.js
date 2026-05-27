5e11315ed04673f12d195107e69abeb615185694
abhicse12 <pandeyabhisbek8@gmail.com>
2026-05-27 12:41:56 +0530

    production api setup

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios"
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+
+  baseURL:
+    "https://transcripto-backend-h9e7.onrender.com/api",
 })
 
 export default API
